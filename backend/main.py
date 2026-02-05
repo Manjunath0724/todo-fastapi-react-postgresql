1ae7a9cf601beadb03f1548b2ac110a1ad6e7289
Manjunath0724 <manjunathgavandi8161@gmail.com>
2026-02-05 11:40:02 +0530

    Added link

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,7 @@ app = FastAPI(title="TaskFlow Pro API", version="1.0.0")
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://todo-fastapi-react-postgresql.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
